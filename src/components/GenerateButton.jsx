ee57664bc59349a0a813362d2d1bb4290a30e171
Jhoann <jhoannegd@gmail.com>
2026-04-06 17:25:13 +0200

    feat:componente GenerateButton listo

--- src/components/GenerateButton.jsx
+++ src/components/GenerateButton.jsx
@@ -1,5 +1,14 @@
+import { RefreshCw } from 'lucide-react'
+
 function GenerateButton() {
-  return <div>GenerateButton</div>
+  return (
+    <div className="w-full max-w-2xl">
+      <button className="w-full flex items-center justify-center gap-3 py-5 rounded-xl bg-linear-to-tl from-[#0e1012] to-[#343a43] border border-[#7eb8d4]/20 text-[#7eb8d4] tracking-widest text-sm font-bold cursor-pointer hover:from-[#114a5d] hover:to-[#343a43] hover:shadow-[0_0_10px_rgba(126,184,212,0.2)] transition-all duration-500">
+        <RefreshCw size={18} />
+        GENERATE PASSWORD
+      </button>
+    </div>
+  )
 }
 
 export default GenerateButton
